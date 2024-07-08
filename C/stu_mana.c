0e43e871fae97b15e46902103eb7012c6a6e57e1
hothanhcong <cong.thanhho@hitachids.com>
2024-07-09 00:37:09 +0700

    update code

--- C/stu_mana.c
+++ C/stu_mana.c
@@ -1,297 +1,235 @@
-/* 
-~ Husen Abugosh (حسين ابوغوش) 
-~ 1210338
-~ lab section: 5L / lecture section: 3
- */
-
-
 // libraries that included in this project :
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// constant that will be  the size of 2 arrays
-#define MAXSIZE 100
-#define NAMELENGTH 50
+// constant that will be the initial size of the student array
+#define INITIAL_SIZE 10
+#define MAXNAMELENGTH 100
 
-// project prtotype function:
+// struct definition for a student
+typedef struct {
+    int id;
+    double avg;
+    char fullName[MAXNAMELENGTH];
+} Student;
+
+// project prototype function:
 void displayMainMenu();
-void addStudent(int ids[], char name[][NAMELENGTH], double avgs[], int *size);
-void removeStudent(int ids[], char name[][NAMELENGTH], double avgs[], int *size);
-void searchForStudent(int ids[], char name[][NAMELENGTH], double avgs[], int  size);
-void uploadDataFile(int ids[], char name[][NAMELENGTH], double avgs[], int  *size );
-void updateDataFile(int ids[], char name[][NAMELENGTH], double avgs[], int  size);
-void printStudents(int ids[], char name[][NAMELENGTH], double avgs[], int  size);
+void addStudent(Student **students, int *size, int *capacity);
+void removeStudent(Student *students, int *size);
+void searchForStudent(Student *students, int size);
+void uploadDataFile(Student **students, int *size, int *capacity);
+void updateDataFile(Student *students, int size);
+void printStudents(Student *students, int size);
 
-// extra prototype function:
-int linear_Search(int a[], int size, int key);
-
-
-// global varibales:
-    int ids[MAXSIZE];
-    double avgs[MAXSIZE];
-    int n;
-    char name[MAXSIZE][NAMELENGTH];
-
-
+// global variables:
+Student *students = NULL;
+int n = 0;
+int capacity = INITIAL_SIZE;
 
 int main() { 
-
-printf("\n\awelcome to my Student Mangement System:\n\n");
-uploadDataFile(ids,name,avgs,&n);
-
-
-return 0;
+    students = (Student*) malloc(INITIAL_SIZE * sizeof(Student));
+    if (students == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    printf("\nWelcome to my Student Management System:\n\n");
+    uploadDataFile(&students, &n, &capacity);
+    return 0;
 }
-
 
 void displayMainMenu() {
+    int choice; 
 
-int choise; 
+    printf("\nPlease Select an operation (1-5):\n");
+    printf("1- Add a Student:\n");
+    printf("2- Remove a Student:\n");
+    printf("3- Search for a Student:\n");
+    printf("4- Print Student List:\n");
+    printf("5- Save Info to File And Exit:\n");
+    printf("\n");
+    scanf("%d", &choice);
 
-printf("\aplease Select an operation (1-4):\n");
-printf("1- Add a Student:\n");
-printf("2- Remove a Student:\n");
-printf("3- Search for a Student:\n");
-printf("4- Print Student List:\n");
-printf("5- Exit System:\n");
-printf("\n");
-scanf("%d", &choise);
-
-
-switch (choise) {
-
- case 1: addStudent(ids, name, avgs,&n);
- break;
-
- case 2: removeStudent(ids, name, avgs,&n);
- break;
-
- case 3: searchForStudent(ids, name, avgs,n);
- break;
-
- case 4: printStudents(ids, name, avgs,n);
- break;
-
- case 5: updateDataFile(ids,name, avgs,n);
- break;
-
-default: 
-printf("No Such operation! Please try again.\n\n");
-displayMainMenu();
- break;
+    switch (choice) {
+        case 1: addStudent(&students, &n, &capacity); break;
+        case 2: removeStudent(students, &n); break;
+        case 3: searchForStudent(students, n); break;
+        case 4: printStudents(students, n); break;
+        case 5: updateDataFile(students, n); break;
+        default: 
+            printf("No such operation! Please try again.\n\n");
+            displayMainMenu();
+            break;
+    }
 }
 
+// addStudent: used to add a new id number, full name, and average:
+void addStudent(Student **students, int *size, int *capacity) {
+    if (*size >= *capacity) {
+        *capacity *= 2;
+        *students = realloc(*students, *capacity * sizeof(Student));
+        if (*students == NULL) {
+            printf("Memory reallocation failed\n");
+            return;
+        }
+    }
 
+    Student newStudent;
+    int isSimilar = 0;
+
+    printf("Please enter id number for Student: \n");
+    scanf("%d", &newStudent.id);
+
+    for(int i = 0; i < *size; i++) {
+        if ((*students)[i].id == newStudent.id) {
+            printf("The id that you are trying to add is similar to another id (please try again!)\n\n");
+            isSimilar = 1;
+            break;
+        }
+    }
+
+    if (isSimilar == 0) {
+        printf("Please enter full name for Student: \n");
+        scanf(" %[^\n]", newStudent.fullName);
+        printf("Please enter average of Student: \n");
+        scanf("%lf", &newStudent.avg);
+
+        (*students)[*size] = newStudent;
+        (*size)++;
+
+        printf("Student with id: %d has been added. \n\n", newStudent.id);
+
+        // Ascending sort by Bubble sort
+        for(int i = 0; i < *size - 1; i++) {
+            for(int j = 0; j < *size - 1 - i; j++) {
+                if ((*students)[j].id > (*students)[j + 1].id) {
+                    Student temp = (*students)[j];
+                    (*students)[j] = (*students)[j + 1];
+                    (*students)[j + 1] = temp;
+                }
+            }
+        }
+    }
+    displayMainMenu();
 }
 
-//  addStudent: used to add a new id number and his average:
-void addStudent(int ids[], char name[][NAMELENGTH], double avgs[], int *size) {
+void removeStudent(Student *students, int *size) {
+    if(*size > 0) {
+        int idNum;
+        int isSimilar = 0;
 
+        printf("Please enter id number for Student to remove: \n");
+        scanf("%d", &idNum);
 
-if(*size < MAXSIZE){                                           // <---- will check if the maxsize(100) > number of student list(n). 
+        for(int i = 0; i < *size; i++) {
+            if (students[i].id == idNum) {
+                isSimilar = 1;
+                for(int j = i; j < *size - 1; j++) {
+                    students[j] = students[j + 1];
+                }
+                (*size)--;
+                printf("Student with id: %d has been removed. \n\n", idNum);
+                break;
+            }
+        }
 
- int idNum;                                                  // <----  used to save the id that added from the user inside it.
- int isSimilar = 0;                                         // <----  bool varibale to check if there any similar value to idNum.
-
-printf("\aPlease enter id number for Student: \n");
-scanf("%d", &idNum);
-
-for(int i=0; i<((*size)-1); i++){                            // <----   linear_Search method to check if is there any similar value to idNum.
-
- if(ids[i] == idNum){
-   printf("the id that you are trying to added is similar to another id (please try again!)\n\n");
-   isSimilar = 1;
-  }
-
+        if(isSimilar == 0) {
+            printf("Student with id %d does not exist.\n\n", idNum);
+        }
+    } else {
+        printf("\nThere are no students to remove \n");
+    }
+    displayMainMenu();
 }
 
-if (isSimilar == 0){                                // <---- if there is no similar value save it in the aaray and take the averge for it.
+void searchForStudent(Student *students, int size) { 
+    if(size > 0) {
+        int idNum;
+        int isSimilar = 0;
 
-  ids[*size-1] = idNum;
-  printf("\aPlease enter name of Student: \n");
-  getchar();
-  fgets(name[MAXSIZE], NAMELENGTH, stdin);
-  name[MAXSIZE][strcspn(name[MAXSIZE], "\n")] = 0; 
-  printf("\aPlease enter avg of Student: \n");
-  scanf("%lf", &avgs[*size-1]);
-  (*size)++ ;
-  printf("Student with id: %d has been added. \n\n", idNum);
+        printf("Please enter id number for Student to search for: \n");
+        scanf("%d", &idNum);
 
+        for(int i = 0; i < size; i++) {
+            if (students[i].id == idNum) {
+                isSimilar = 1;
+                printf("id# = %d \t\t avg = %.2f \t\t name = %s\n\n", students[i].id, students[i].avg, students[i].fullName);
+                break;
+            }
+        }
+
+        if(isSimilar == 0) {
+            printf("Student with id %d does not exist (you can add one).\n\n", idNum);
+        }
+    } else {
+        printf("\nThere are no students to search for \n");
+    }
+    displayMainMenu();
 }
 
-// ascendig sort by Bubble sort (it should be with Shift method!!)  !!!!!!!!!!!!!
-for(int i = 0; i<(*size)-1; i++){
-
- for(int j=0; j< ((*size)-2); j++){
-
-     if(ids[j]>ids[j+1]){
-    
-     int temp = ids[j];
-     ids[j] = ids[j+1];
-     ids[j+1] = temp;
-
-     int temp2 = avgs[j];
-     avgs[j] = avgs[j+1];
-     avgs[j+1] = temp2;
-
-   //   char temp3 = name[j];
-   //   name[j][] = name[j+1];
-   //   name[j+1] = temp3;
-     }
-  }
-}
-
-}else{
-// if the maxsize(100) is *LESS* than  THE number of student list(n)  IT will show the error meesage below:  
-printf("\n the file is Full try to remove ids or upradge to our new version! \n\n");
-}
-      displayMainMenu();
-
-}
-
-
-
-
-
-void removeStudent(int ids[], char name[][NAMELENGTH], double avgs[], int *size){
-      
-if(*size > 0){
-
- int idNum;                                                  // <----  used to save the id that added from the user inside it.
- int isSimilar = 0;                                         // <----  bool varibale to check if there any similar value to idNum.
-
-printf("\aPlease enter id number for Student to remove: \n");
-scanf("%d", &idNum);
-
-for(int i=0; i<MAXSIZE; i++){                            // <----   linear_Search method to check if is there any similar value to idNum.
-
- if(ids[i] == idNum){
-
-      isSimilar = 1;
-
-   if (i < (MAXSIZE -1)){                          // <---- remove shift method.
-  
-       for (int j = i; j < (MAXSIZE-1); j++)
-       {
-           ids[j] = ids[j+1];
-           avgs[j] = avgs[j+1];
-       }
-   }
-   (*size)--;
-   printf("Student with id: %d has been removed. \n\n",idNum);
-  }
-
-}
-
-if (isSimilar == 0){                                // <---- if there is no similar value save it in the aaray and take the averge for it.
- printf("Student with %d id does not exist.\n\n", idNum);
-}
-
- }else{
-// if the file is empty show that message error:
-printf("\nthere is no ids student to remove \n");
-
-}
-      displayMainMenu();
-
-}
-
-
-
-
-void searchForStudent(int ids[], char name[][NAMELENGTH], double avgs[], int  size){ 
-
-
- if(size > 0){
-
- int idNum;                                                  // <----  used to save the id that added from the user inside it.
- int isSimilar = 0;                                         // <----  bool varibale to check if there any similar value to idNum.
-
-printf("\aPlease enter id number for Student to search for : \n");
-scanf("%d", &idNum);
-
-for(int i=0; i<MAXSIZE; i++){                            // <----   linear_Search method to check if is there any similar value to idNum.
-
- if(ids[i] == idNum){
-      isSimilar = 1;
-      printf("id# = %d \t\t avg = %.2f\n\n",ids[i],avgs[i]);  //<----- will print the id and his avg.
-   }
-   size--;
-  }
-
- if (isSimilar == 0){                                // <---- if there is no similar value save it in the aaray and take the averge for it.
- printf("Student with %d id does not exist (you can add one).\n\n", idNum);
-}
-
-} else{
-// if the file is empty show that message error:
-printf("\nthere is no ids student to search for \n");
-}
-
-      displayMainMenu();
-
-}
-
-
-
-void uploadDataFile (int ids[], char name[][NAMELENGTH], double avgs[], int  *size ){
-  
+void uploadDataFile(Student **students, int *size, int *capacity) {
     printf("Uploading data file ... \n");
 
-     FILE *input;                                 // <--- will declare a file pointer variable.
-     input = fopen("students.txt","r");          // <--- will call a function that open the file and put it in the reading mode
-     int idInput = 0;                           //
-     double avgInput = 0.0;                    //  <--- varibales
-     int lines = 0;                           
+    FILE *input; 
+    input = fopen("students.txt", "r");
+    if (input == NULL) {
+        printf("Could not open file\n");
+        return;
+    }
 
-    /* so here there will be reading all the data(id and the avrage) in the file & save it in there varibales then will save the avg data in avg[] and the ids data in id[]
-    until the end of file :
-    */
-    while (fscanf(input, "%d %lf", &idInput, &avgInput) != EOF)
-    {
-        ids[lines] = idInput;
-        avgs[lines] = avgInput;
+    int idInput;
+    double avgInput;
+    char nameInput[MAXNAMELENGTH];
+    int lines = 0;
+
+    while(fscanf(input, "%d %lf %[^\n]", &idInput, &avgInput, nameInput) != EOF) {
+        if (lines >= *capacity) {
+            *capacity *= 2;
+            *students = realloc(*students, *capacity * sizeof(Student));
+            if (*students == NULL) {
+                printf("Memory reallocation failed\n");
+                fclose(input);
+                return;
+            }
+        }
+        (*students)[lines].id = idInput;
+        (*students)[lines].avg = avgInput;
+        strcpy((*students)[lines].fullName, nameInput);
         lines++;
     }
 
-    *size = lines + 1;
-    printf("Data File uploaded \n \n");
+    *size = lines;
+    fclose(input);
+    printf("Data File uploaded \n\n");
     displayMainMenu();
-
 }
 
-void updateDataFile(int ids[], char name[][NAMELENGTH], double avgs[], int size){
-
+void updateDataFile(Student *students, int size) {
     printf("\nUpdating data file ... \n");
 
-    FILE *output;                                 // <--- will declare a file pointer variable.
-    output = fopen("students.txt","w");          // <--- will call a function that open the file and put it in the wirting mode.
-    int idOutput = 0;                           //
-    double avgOutput = 0.0;                    //  <--- varibales
-    int i = 0; 
+    FILE *output;
+    output = fopen("students.txt", "w");
+    if (output == NULL) {
+        printf("Could not open file\n");
+        return;
+    }
 
-    while (i < (size-1))
-    {
-        idOutput = ids[i];
-        avgOutput = avgs[i];
-        fprintf(output, "%d %.2lf \n", idOutput, avgOutput);      // <---- will print the ids[] values and avgs[] values in the  (students.txt) file
-        i++;
+    for(int i = 0; i < size; i++) {
+        fprintf(output, "%d %.2lf %s\n", students[i].id, students[i].avg, students[i].fullName);
     }
     fclose(output);
-    
+
     printf("Data File updated. \n\n");
-    printf("Thank you for using My Student Management System. GoodBye :)\n\n");
-   
+    printf("Thank you for using My Student Management System. Goodbye :)\n\n");
+
+    // Free the allocated memory
+    free(students);
 }
 
-
-
-void printStudents(int ids[], char name[][NAMELENGTH], double avgs[], int  size) {
-      
-// it will print all the ids and avgs values 
-    for (int i = 0; i < size; i++) {
-        printf("id# = %d \t\t name: %s \t\t avg = %.2f\n", ids[i], name[i], avgs[i]);
+void printStudents(Student *students, int size) {
+    for(int i = 0; i < size; i++) {   
+        printf("id# = %d \t\t avg = %.2f \t\t name = %s\n", students[i].id, students[i].avg, students[i].fullName);
     }
     printf("\n");
     displayMainMenu();
